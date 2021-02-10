187adbb633b58ecfdd981a485503257c4940c864
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-10 00:44:17 -0500

    feat: Add handlebars for show views in api response

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
+const path = require('path')
 
 const app = express()
 
 var port = 3000
+
+app.set('views', path.join(__dirname, 'views'))
+
+// show views in api response
+app.engine('.hbs', handlebars({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}))
+
+app.set('view engine', '.hbs')
 
 app.use(require('./routes/index.controller'))
 
