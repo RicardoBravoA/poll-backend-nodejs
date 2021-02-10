515436565fd6a0a1c0cd0470f0d73e045d5ac021
Ricardo Bravo <ricardobravo@outlook.com>
2021-02-09 23:50:52 -0500

    feat: Add index controller

---
+++ src/routes/index.controller.js
@@ -0,0 +1,11 @@
+const { request, response } = require('express')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (request, response) => {
+    response.send('Hello World')
+})
+
+module.exports = {
+    router
+}
